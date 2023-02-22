2f2e8ebd1af2aad406dbc4f549105318ad55e4ba
Ronald-patrick <ronald.patrick1101@gmail.com>
2023-02-22 22:00:45 +0530

    added directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,9 +2,33 @@ import { Component } from "@angular/core";
 
 @Component({
     selector : 'app-server',
-    templateUrl : './server.component.html'
+    templateUrl : './server.component.html',
+    styles : [`
+        .status {
+            color : white
+        }
+        .online {
+            background-color : green
+        }
+        .offline {
+            background-color : red
+        }
+    `]
 })
 
 export class ServerComponent {
+    serverId : number = 10;
+    serverStatus : string = 'offline';
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red'
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,12 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
+  templateUrl : './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer : boolean = false;
+  serverCreationStatus : string = 'No server was created!'
+  serverName = ''
+  serverCreated = false;
+  servers = ['Mumbai Server','Pune Server'];
+
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },1500);
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+  onUpdateServerName(e : Event){
+    this.serverName = (<HTMLInputElement> e.target).value
+  }
 
 }
